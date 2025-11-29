ae0a863e0ab45413cb6bc8123ac85bbd118dbc25
dailyatti <dailyatti@gmail.com>
2025-11-29 12:58:42 +0100

    Fix Netlify build: Downgrade Tailwind/Vite, fix CSS import, standard config

---
+++ debug-tailwind.js
@@ -0,0 +1,5 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+console.log('Tailwind Result:', tailwindcss());
+console.log('Autoprefixer Result:', autoprefixer());
