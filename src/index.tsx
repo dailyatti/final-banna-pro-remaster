6d7e82ec7d1bc5dbbab31d5ab5d22d780d30d673
dailyatti <dailyatti@gmail.com>
2025-11-29 14:32:56 +0100

    Fix: Wrap App in ApiKeyProvider to prevent runtime crash

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,12 @@ import App from './MainApp'
 import './index.css'
 import './services/i18n' // Import i18n configuration
 
+import { ApiKeyProvider } from './context/ApiKeyContext'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <ApiKeyProvider>
+            <App />
+        </ApiKeyProvider>
     </React.StrictMode>,
 )
